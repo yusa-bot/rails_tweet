6b4367c0119f0665c14550a9a6068f745b945fd6
yusa <yusaaihara@gmail.com>
2025-03-20 22:17:46 +0900

    2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
 end
